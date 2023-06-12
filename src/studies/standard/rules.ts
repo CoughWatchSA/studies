9056fac588ffa6435b9dee028d8b31f07c64932d
Matteo Delfino <matteo.delfino@isi.it>
2023-06-12 08:23:31 +0200

    update standard study rules

--- src/studies/standard/rules.ts
+++ src/studies/standard/rules.ts
@@ -2,9 +2,11 @@ import { StudyRules } from "case-editor-tools/types/studyRules";
 import { StudyEngine } from "case-editor-tools/expression-utils/studyEngineExpressions";
 import { Intake } from "./surveys/intake";
 import { Expression } from "survey-engine/data_types";
+import { Vaccination } from "./surveys/vaccination";
 
 const entryRules: Expression[] = [
-    StudyEngine.participantActions.assignedSurveys.add(Intake.surveyKey, 'normal')
+    StudyEngine.participantActions.assignedSurveys.add(Intake.surveyKey, 'normal'),
+    StudyEngine.participantActions.assignedSurveys.add(Vaccination.surveyKey, 'normal')
 ];
 
 
